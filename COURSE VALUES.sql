464df3503df053afe54beada20bcdae35690435c
emmanuelmeg <chimereleo151@gmail.com>
2026-01-09 00:16:41 +0000

    Add files via upload

---
+++ COURSE VALUES.sql
@@ -0,0 +1,33 @@
+INSERT INTO course VALUES (
+'NURS12', ' Nursing','SF102','3 years','DP004'
+);
+
+INSERT INTO Course VALUES (
+'MEDLAB13','medical Laboratory','SF109','4 years','DP004'
+);
+
+INSERT INTO course VALUES (
+'PG267', 'PgDIT','SF105','2 years','DP006'
+);
+
+INSERT INTO course VALUES (
+'TCH24', ' teaching','SF109', '3 years','DP006'
+);
+
+INSERT INTO course VALUES (
+'MENG34',' Mechaincal Engineering  ','SF114','4 years','DP001'
+);
+
+INSERT INTO course VALUES (
+'CSENG36', 'Computer Engineering','SF112','4 years', 'DP001'
+);
+
+INSERT INTO course VALUES (
+'COMSIC242', 'Computer Science ','SF104','3 years','DP002'
+);
+
+INSERT INTO course VALUES (
+'ML679','Media Literacy','SF110', '3 Years','DP007'
+);
+
+COMMIT;

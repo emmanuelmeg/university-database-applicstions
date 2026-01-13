02a6333f36b583d8a28b47522a2dc969f51a4db2
emmanuelmeg <chimereleo151@gmail.com>
2026-01-13 13:52:03 +0000

    Add module result entries to the database
    Inserted multiple records into the module_result table with various statuses.

---
+++ MODULE RESLUT VALUES.sql
@@ -0,0 +1,48 @@
+INSERT INTO module_result VALUES (
+'01/5023','MM5002','PASS'
+);
+ INSERT INTO module_result VALUES(
+ '00/5089','EH5029','FAIL'
+ );
+ INSERT INTO module_result VALUES(
+ '00/9832', 'EH5029','PASS'
+ );
+ INSERT INTO module_resulte VALUES (
+ '03/3678', 'ML3006', 'PASS'
+ );
+ 
+ INSERT INTO module_result VALUES (
+ '02/7809','TD4067','FAIL'
+ );
+ 
+ INSERT INTO module_result VALUES (
+ '00/8392','MB5078','PASS'
+ );
+ 
+INSERT INTO module_result VALUES (
+'00/8724','ANT6089','PASS' 
+ );
+ 
+ INSERT INTO module_result VALUES (
+ '00/8790','TD4067','PASS'
+ );
+ INSERT INTO module_result VALUES (
+'00/9456','CS4005','FAIL'
+ );
+ 
+ INSERT INTO module_result VALUES(
+ '02/2893','SA4002','PASS'
+ );
+ INSERT INTO module_result VALUES(
+'03/3678','MM5002','FAIL');
+
+INSERT INTO module_result VALUES(
+  '02/2893','SA4002','PASS');
+
+  INSERT INTO module_result Values(
+  '03/3678', 'MM5002','FAIL');
+
+COMMIT
+
+ 
+ COMMIT;
